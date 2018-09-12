1fbc627d5824eecb9c2087091be18d322ab69ae7
sergiohc <sergioh.c@live.com>
2018-09-11 22:39:20 -0300

    suply models

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,7 +1,8 @@
 class Channel < ApplicationRecord
- has_many :messages, as: :messagable, :dependent => :destroy
+  has_many :messages, as: :messagable, :dependent => :destroy
   belongs_to :team
   belongs_to :user
   validates_presence_of :slug, :team, :user
-  validates :slug, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :slug, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates_uniqueness_of :slug, :scope => :team_id
 end
