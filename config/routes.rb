b2f5415dd673050f08164288a0bc5fe209c5e826
sergiohc <sergioh.c@live.com>
2018-09-18 17:58:33 -0300

    controllers teams, channels, view channel jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'channels/create'
+
+  get 'channels/destroy'
+
+  get 'channels/show'
+
+  get 'teams/index'
+
+  get 'teams/show'
+
+  get 'teams/create'
+
+  get 'teams/destroy'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
