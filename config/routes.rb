79317f6a46f7de32deb2ac3bc0f9524f12d4c691
sergiohc <sergioh.c@live.com>
2018-09-24 00:36:43 -0300

    set conf redis, adjust sidebar and set action cable

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :talks, only: [:show]
   resources :team_users, only: [:create, :destroy]
   devise_for :users, :controllers => { registrations: 'registrations' }
+
+  mount ActionCable.server => '/cable'
 end
